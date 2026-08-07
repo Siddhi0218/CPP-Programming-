1fe0938405711e5f58aae22967ce794aa04792d0
Siddhi0218 <siddhilole18@gmail.com>
2026-08-07 05:09:14 +0000

    changes

---
+++ employee.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Employee
+{
+    int employeeID;
+    string name;
+    float salary;
+
+public:
+    void input()
+    {
+        cout << "Enter Employee ID:";
+        cin >> employeeID;
+        cout << "Eneter Employee Name:";
+        cin >> name;
+        cout << "Enter Salary:";
+        cin >> salary;
+    }
+    void output()
+    {
+        cout << "Employee ID : " <<employeeID<<endl;
+        cout << "Name : " <<name<<endl;
+        cout << "Salary : " <<salary<<endl;
+    }
+    };
+    int main()
+    {
+        Employee e;
+        e.input();
+        e.output();
+        return 0;
+    }
