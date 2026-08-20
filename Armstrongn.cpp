4c335ffd4cf2cab7006cf13ef8995eee6cdf634b
Siddhi0218 <siddhilole18@gmail.com>
2026-08-20 19:17:27 +0000

    changes

---
+++ Armstrongn.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, original, remainder, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    original = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        sum = sum + (remainder * remainder * remainder);
+        n = n / 10;
+    }
+
+    if (sum == original)
+        cout << original << " is an Armstrong number.";
+    else
+        cout << original << " is not an Armstrong number.";
+
+    return 0;
+}
