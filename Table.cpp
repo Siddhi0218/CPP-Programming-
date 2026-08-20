b3ee68a4c7bb41451adb5931f561919c82350ebb
Siddhi0218 <siddhilole18@gmail.com>
2026-08-20 16:33:18 +0000

    changes

---
+++ Table.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout<< "Enter a number to calculate the table :";
+    cin>> a;
+
+    for(int i=1; i<=10; i++)
+    {
+        cout<<a<<"*"<<i<<"="<<a*i<<"\n";
+    }
+}
